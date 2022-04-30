ffda252d5f6803c2e7c581b1239cbd7c3136f714
Alexandru Dumitru <alexandru666@outlook.es>
2022-04-30 18:09:41 +0200

    procedural Generation

--- Assets/BGScroll.cs
+++ Assets/BGScroll.cs
@@ -6,6 +6,9 @@ public class BGScroll : MonoBehaviour
 {
     public float speed = 4f;
     private Vector3 StartPosition;
+
+    [SerializeField] GameObject fondoPantalla;
+    [SerializeField] GameObject platform;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +22,36 @@ public class BGScroll : MonoBehaviour
         if (transform.position.y < -13.50f)
         {
             transform.position = StartPosition;
+            movePlatforms();
         }
+    }
+
+    void generateProceduralWorld(GameObject newP)
+    {
+        for (int i = 0; i < 14; ++i)
+        {
+            GameObject p = Instantiate(platform, new Vector3(0, 0, -1), new Quaternion());
+            p.transform.parent = newP.transform;
+
+            p.transform.localPosition = new Vector3(-1.0f + (Random.value*2.0f), (float)i - 1.5f, -1);
+        }
+    }
+
+    void movePlatforms()
+    {
+        GameObject oldP = transform.Find("oldPlatforms").gameObject;
+        GameObject newP = transform.Find("newPlatforms").gameObject;
+
+        Destroy(oldP);
+
+        newP.transform.Translate(new Vector3(0, -13.5f, 0), Space.World);
+        newP.name = "oldPlatforms";
+        oldP = newP;
+
+        newP = new GameObject("newPlatforms");
+        newP.transform.localPosition = new Vector3(0, 8.5f, 0);
+        newP.transform.parent = transform;
+
+        generateProceduralWorld(newP);
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,10 +4,6 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
     // Start is called before the first frame update
     void Start()
     {
